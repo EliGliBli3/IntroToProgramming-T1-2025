54fb72a8068cdf5f1444cd65f8e251a850367765
EliGliBli3 <elinotellie3@gmail.com>
2025-09-25 13:52:35 -0500

    Added SpaceShuttleSupply.py

---
+++ Assignments/SpaceShuttleSupply.py
@@ -0,0 +1,18 @@
+import os
+
+os.system('cls')
+print("Preparing for takeoff. Please confirm the following:\n")
+countdown = input("Seconds until launch > ")
+os.system('cls')
+
+print("Please provide the quantities aboard for the following:")
+oxygen_tanks = input("\nOxygen tanks > ")
+food_packs = input("Food Packs > ")
+water_packs = input("Water Packs > ")
+
+os.system('cls')
+print(f"Entered supplies:\nOxygen Tanks: {oxygen_tanks}\nFood Packs: {food_packs}\nWater Packs: {water_packs}")
+oxygen_tanks = input("\nPlease reconfirm oxygen tank supply.\n> ")
+
+os.system('cls')
+print(f"All supplies confirmed:\nOxygen Tanks: {oxygen_tanks}\nFood Packs: {food_packs}\nWater Packs: {water_packs}\n\nTime until takeoff: {countdown}")
---
+++ Practice/2.3-practice.py
@@ -0,0 +1,2 @@
+num = input("What number would you like to square?\n> ")
+print(f"{num}^2 = {float(num)**2}")
