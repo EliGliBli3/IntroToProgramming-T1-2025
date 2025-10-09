94a92cd941e5b025e152c6dc2b3902f12d1013ee
EliGliBli3 <elinotellie3@gmail.com>
2025-10-09 14:11:54 -0500

    Added "3.3-Elif Statements.py"

---
+++ Practice/3.3-Elif Statements.py
@@ -0,0 +1,17 @@
+#   String functions practice
+
+message = input("Enter sentence\n>")
+
+print(message.upper())
+print(message.strip())
+print(message.replace("bad", "good"))
+print(message.endswith('.'))
+
+
+
+#   Elif practice
+
+coin_diameter = 4
+deposit = 0.0
+
+if coin_diameter == 
