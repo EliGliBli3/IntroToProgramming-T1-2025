4b60ad1f5edc50978c407b56ba6cea6b4e5cfe45
EliGliBli3 <elinotellie3@gmail.com>
2025-11-05 13:30:30 -0600

    Completed 4.1 practice

---
+++ Practice/4.1-Lists.py
@@ -0,0 +1,21 @@
+import os
+
+fav_fruits = ["apple", "Apple", "banana", "orange", "Banana"]
+print(fav_fruits[0])
+print(fav_fruits[::-1][0])
+
+fav_fruits.append(input("Add a fruit to the list\n>"))
+def prompt_remove():
+    try:
+        remove = input(f"Select item to remove:\n{"\n".join(fav_fruits)}\n\n>")
+        fav_fruits.remove(remove)
+    except:
+        os.system('cls')
+        prompt_remove()
+prompt_remove()
+
+fav_fruits.sort()
+print(f"\nApple appears {[f.lower() for f in fav_fruits].count("apple")} times.\n")
+
+for f in fav_fruits:
+    print(f)
